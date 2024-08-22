737adb1a3733063f0e21abf8d89198768ddecfd1
Ekin KARAKOÇ <ekinkarakoc1@gmail.com>
2024-08-22 14:02:29 +0300

    contribution-deneme

--- app/archive/layout.js
+++ app/archive/layout.js
@@ -3,8 +3,7 @@ import React from "react";
 const ArchiveLayout = ({ archive, latest }) => {
   return (
     <div>
-      <h1>News Archive</h1>
-
+      <h1>News deneme Archive</h1>
       <section id="archive-filter">{archive}</section>
       <section id="archive-latest">{latest}</section>
     </div>
